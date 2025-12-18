76390c37c9fa86a75c13e774a277eaa2844d2dd1
ighook <jinbeom4292@gmail.com>
2025-12-18 15:04:29 +0900

    젠킨스 테스트

--- client/src/layouts/Sidebar.tsx
+++ client/src/layouts/Sidebar.tsx
@@ -4,7 +4,7 @@ import styles from "./Sidebar.module.css";
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <span className={styles.header}>Sensor Monitor</span>
+      <span className={styles.header}>Sensor Monitoring</span>
       <nav>
         <ul>
           <li>
